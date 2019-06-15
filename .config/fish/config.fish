f82869b5dbd71e79a25dae483d4192af4b7cfaa1
Kerem Goksel <bkgoksel@gmail.com>
2019-06-14 18:41:07 -0700

    Add rainbow header

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,4 @@ set -x PATH $PATH /mnt/c/Users/bkgoksel/.local/bin
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 set -x EDITOR vim
 bass (dircolors ~/.dircolors.ansi-dark)
+alias clear "clear; echo; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo"
